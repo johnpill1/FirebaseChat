3fbadc18f17ae93c2257f472589a78f8f7ce59f1
John Pill <john_pill1@hotmail.com>
2022-08-10 17:18:32 +0100

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by John Pill on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
